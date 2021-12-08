712828af2aa9b1629b32db014eb8dcf56adc88cb
EmreKse <emreqse@gmail.com>
2021-12-09 01:08:43 +0300

    ea

--- src/webpages/Faculty.js
+++ src/webpages/Faculty.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect }  from 'react';
-import {FacultyService} from "../service/FacultyService";
+import './Faculty.css';
+import React, { useState, useEffect } from 'react';
+import { FacultyService } from "../service/FacultyService";
 const Faculty = () => {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
@@ -24,15 +25,42 @@ const Faculty = () => {
         return <div>Loading...</div>;
     } else {
         return (
-            <div>
-                <h1>Faculties</h1>
-                <ul>
-                    {faculties.map(faculty => (
-                        <li key={faculty.id}>
-                            {faculty.name}
-                        </li>
-                    ))}
-                </ul>
+            <div id="container">
+                <h2>Faculties</h2>
+                <div id="addFac">
+                    <label htmlfor="facName">Faculty Name: </label>
+                    <input type="text" id="facName" placeholder="Faculty Name" />
+                    <label htmlfor="deanName">Dean Name: </label>
+                    <input type="text" id="deanName" placeholder="Dean Name" />
+                    <button>Add Faculty</button>
+                </div>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Faculty Name</th>
+                                <th>Dean</th>
+                                <th>Selection</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {faculties.map(faculty => (
+                                <tr>
+                                    <td key={faculty.id}>
+                                        {faculty.name}
+                                    </td>
+                                    <td key={faculty.id}>
+                                        {faculty.dean}
+                                    </td>
+                                    <td>
+                                        <button>Edit</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         );
     }
